ee4e3090b08fdc69cae1f593ca0d187341f72bea
Michael Owings <mikey@swampgas.com>
2014-07-25 11:21:34 -0500

    grab.py

---
+++ grab.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+import io
+import time, datetime
+import picamera
+import cv2
+import numpy as np
+
+
+# A simple Motion Detection algorithm.
+class MotionDetection:
+    def __init__(self):
+        self.__image0 = None
+        self.__image1 = None
+        self.__image2 = None
+        self._count = 0
+
+        # Configurations
+        # Change these to adjust sensitive of motion
+        self._MOTION_LEVEL = 2000
+        self._THRESHOLD = 35
+
+    def _updateImage(self, image):
+        self.__image2 = self.__image1
+        self.__image1 = self.__image0
+        self.__image0 = image
+
+    def _ready(self):
+        return self.__image0 != None and self.__image1 != None and self.__image2 != None
+
+    def _getMotion(self):
+        if not self._ready():
+            return None
+
+        d1 = cv2.absdiff(self.__image1, self.__image0)
+        d2 = cv2.absdiff(self.__image2, self.__image0)
+        result = cv2.bitwise_and(d1, d2)
+
+        (value, result) = cv2.threshold(result, self._THRESHOLD, 255, cv2.THRESH_BINARY)
+
+        scalar = cv2.sumElems(result)
+
+        # print " - scalar:", scalar[0], scalar
+        return scalar
+
+    def detectMotion(self, image):
+        self._updateImage(image)
+
+        motion = self._getMotion()
+        if motion and motion[0] > self._MOTION_LEVEL:
+            print "motion scalar:", motion[0], motion
+            return True
+        return False
+
+    def saveImage(self, camera):
+        tstmp = datetime.datetime.now().strftime("%Y-%m-%d-%H%M%S")
+        image_file  = "/opt/camera/motion/capture_%s_%05d.jpg" % (tstmp, self._count)
+        camera.resolution = (1944, 1944)
+        camera.capture(image_file, format='jpeg', use_video_port = False, quality = 100)
+        print "  - Image saved:", image_file
+        camera.resolution = (150,150)
+        thumb_image_file  = "/opt/camera/motion/thumb_%s_%05d.jpg" % (tstmp, self._count)
+        cv2.imwrite(thumb_image_file, self.__image0)
+        self._count += 1
+
+def process():
+    print "Initializing camera..."
+    with picamera.PiCamera() as camera:
+        camera.rotation = 270
+        camera.resolution = (150,150)
+        camera.exposure_mode = 'auto'
+        camera.awb_mode = 'auto'
+        camera.brightness = 50
+        print "Setting focus and light level on camera..."
+        time.sleep(2)
+
+        print "Initializing the CameraDetection..."
+        detection = MotionDetection()
+
+        count = 0
+
+        while True:
+            # Create the in-memory stream
+            stream = io.BytesIO()
+            camera.capture(stream, use_video_port=True, format='jpeg')
+
+            # Construct a numpy array from the stream
+            data = np.fromstring(stream.getvalue(), dtype=np.uint8)
+            # "Decode" the image from the array, preserving colour
+            image = cv2.imdecode(data, 1)
+            if detection.detectMotion(image):
+                print "Motion detected"
+                detection.saveImage(camera)
+
+if __name__ == "__main__":
+    process()
