77e357698382ba626a19a4f374677bb12d747c97
Michael Owings <mikey@swampgas.com>
2014-07-25 23:00:59 -0500

    motion refactor

--- grab.py
+++ grab.py
@@ -5,76 +5,64 @@ import picamera
 import cv2
 import numpy as np
 
-
-# A simple Motion Detection algorithm.
-class MotionDetection:
-    def __init__(self):
-        self.__image0 = None
-        self.__image1 = None
-        self.__image2 = None
-        self._count = 0
-
-        # Configurations
-        # Change these to adjust sensitive of motion
-        self._MOTION_LEVEL = 2000
-        self._THRESHOLD = 35
-
-    def _updateImage(self, image):
-        self.__image2 = self.__image1
-        self.__image1 = self.__image0
-        self.__image0 = image
+class MotionDetector:
+    def __init__(self, change_threshold, threshold):
+        self.images = []
+        self._change_threshold = change_threshold
+        self._threshold = threshold
+        self.motion = None
+        self.bitwise_and = None
+        self.total=0
+        self.mean=0.0
+        self.stddev=0.0
 
     def _ready(self):
-        return self.__image0 != None and self.__image1 != None and self.__image2 != None
+        return len(self.images) == 3
 
-    def _getMotion(self):
-        if not self._ready():
-            return None
+    def _new_image(self, image):
+        self.images.insert(0, image)
+        if len(self.images) > 3: self.images.pop()
 
-        d1 = cv2.absdiff(self.__image1, self.__image0)
-        d2 = cv2.absdiff(self.__image2, self.__image0)
-        result = cv2.bitwise_and(d1, d2)
+    def _get_motion(self):
+        if not self._ready(): return None
+        diff1 = cv2.absdiff(self.images[1], self.images[0])
+        diff2 = cv2.absdiff(self.images[2], self.images[0])
+        self.bitwise_and  = cv2.bitwise_and(diff1, diff2)
+        dummy, self.motion  =  cv2.threshold(self.bitwise_and, self._threshold, 255, cv2.THRESH_BINARY)
+        self.total  = cv2.sumElems(self.motion)[0]
+        scalar_mean, scalar_stddev = cv2.meanStdDev(self.motion)
+        self.mean = scalar_mean[0][0]
+        self.stddev = scalar_stddev[0][0]
+        return self.total
 
-        (value, result) = cv2.threshold(result, self._THRESHOLD, 255, cv2.THRESH_BINARY)
-
-        scalar = cv2.sumElems(result)
-
-        # print " - scalar:", scalar[0], scalar
-        return scalar
-
-    def detectMotion(self, image):
-        self._updateImage(image)
-
-        motion = self._getMotion()
-        if motion and motion[0] > self._MOTION_LEVEL:
-            print "motion scalar:", motion[0], motion
+    def detect_motion(self, image):
+        self._new_image(image)
+        total = self._get_motion()
+        if total and total > self._change_threshold:
+            print "Sum:", self.total
+            print "Mean:", self.mean
+            print "Std. Dev:", self.stddev
             return True
-        return False
+        else:
+            return False
 
-    def saveImage(self, camera):
-        tstmp = datetime.datetime.now().strftime("%Y-%m-%d-%H%M%S")
-        image_file  = "/opt/camera/motion/capture_%s_%05d.jpg" % (tstmp, self._count)
-        camera.resolution = (1944, 1944)
-        camera.capture(image_file, format='jpeg', use_video_port = False, quality = 100)
-        print "  - Image saved:", image_file
-        camera.resolution = (150,150)
-        thumb_image_file  = "/opt/camera/motion/thumb_%s_%05d.jpg" % (tstmp, self._count)
-        cv2.imwrite(thumb_image_file, self.__image0)
-        self._count += 1
+def mk_filename(basename, count):
+     return "/opt/camera/motion/%s_%s_%05d.jpg" % (basename, datetime.datetime.now().strftime("%Y-%m-%d-%H%M%S"), count)
 
 def process():
     print "Initializing camera..."
     with picamera.PiCamera() as camera:
         camera.rotation = 270
         camera.resolution = (150,150)
-        camera.exposure_mode = 'auto'
+        camera.exposure_mode = 'night'
         camera.awb_mode = 'auto'
-        camera.brightness = 50
+        camera.brightness = 60
+        camera.color_effect=(128, 128)
         print "Setting focus and light level on camera..."
         time.sleep(2)
 
         print "Initializing the CameraDetection..."
-        detection = MotionDetection()
+        detector = MotionDetector(5000, 15)
 
         count = 0
 
@@ -86,10 +74,20 @@ def process():
             # Construct a numpy array from the stream
             data = np.fromstring(stream.getvalue(), dtype=np.uint8)
             # "Decode" the image from the array, preserving colour
-            image = cv2.imdecode(data, 1)
-            if detection.detectMotion(image):
-                print "Motion detected"
-                detection.saveImage(camera)
+            image = cv2.imdecode(data, cv2.CV_LOAD_IMAGE_GRAYSCALE)
+            if detector.detect_motion(image):
+                image_file =  mk_filename('motion', count)
+                cv2.imwrite(image_file, detector.motion)
+                image_file =  mk_filename('frame', count)
+                cv2.imwrite(image_file, detector.images[1])
+                image_file = mk_filename('mask', count)
+                cv2.imwrite(image_file, detector.bitwise_and)
+                print "Motion detected. Frame: ", count
+                print "    Sum:", detector.total
+                print "    Mean:", detector.mean
+                print "    Std. Dev:", detector.stddev
+     
+                count += 1
 
 if __name__ == "__main__":
     process()
